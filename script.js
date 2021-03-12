86c3022dde17e72c54bf2d814354f93abebeb66a
Antonia Pettersson <antoniapettersson@Antonias-MacBook-Pro.local>
2021-03-12 15:17:34 +0100

    Css

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("hej");
+
+// ------ hämta från DOM ----- //
+const content = document.querySelector(".content");
+
+// ----- create element ----- //
+const ul = document.createElement("ul");
+ul.setAttribute("class", "myUl");
+content.appendChild(ul);
+
+// ------ fetch ----- //
+fetch("/adress.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    printInfo(data);
+  });
+
+function printInfo(persons) {
+  for (person in persons) {
+    tamplate = `<li class"myli> Namn:  ${persons[person].Namn}<br> Emal: ${persons[person].email}<br>  GitHub: ${persons[person].gitHub} <br> Stad:  ${persons[person].stad} </li> <br>`;
+
+    ul.insertAdjacentHTML("beforeend", tamplate);
+  }
+}
